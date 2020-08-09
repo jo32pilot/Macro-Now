a3579d6d207add1c9d3852796fe4775b21d5941b
John Li <john166@rocketmail.com>
2020-08-09 16:50:48 -0700

    this one too

--- KeyWatcher.py
+++ KeyWatcher.py
@@ -1,40 +1,75 @@
-from pyHook import HookManager
-from pyHook import HookConstants
 from win32gui import PostQuitMessage
-from PyQt5.QtWidgets import QListWidgetItem
 from StepConstants import StepEnum
+from pynput import mouse, keyboard
+from threading import Thread, Lock
+import time
+
+# maybe can turn this all into snake case?
+
+# what will happen to thread if not yet stopped and delete a step?
+
 
 class KeyWatcher():
 
-    def __init__(self, window, currentTime, diffTime, totalTime):
+    def __init__(self, listWidget, currentTime, diffTime, totalTime):
+        self.listWidget = listWidget
         self.currentTime = currentTime
         self.diffTime = diffTime
         self.totalTime = totalTime
+        self.keysDown = dict()
 
-        self.hm = HookManager()
-        self.hm.HookKeyboard()
-        self.hm.HookMouse()
+        self.listWidget.keyPress.connect(self.onPress)
+        self.listWidget.keyRelease.connect(self.onRelease)
 
-        self.hm.SubscribeKeyAll(self.on_keyboard_event)
-        #self.hm.SubscribeMouseAll()
+        self.updater = Thread()
+        self.lock = Lock()
 
-        self.window = window
+        self.keyController = keyboard.Controller()
 
-    def on_key_up_event(self, event):
-        print(event.KeyID)
+        self.keyboard = keyboard.Listener(on_press=self.onPressEmit, 
+                on_release=self.onReleaseEmit)
+        self.keyboard.start()
+
+    def onPressEmit(self, key):
+        self.listWidget.keyPress.emit(key)
+
+    def onReleaseEmit(self, key):
+        self.listWidget.keyRelease.emit(key)
+
+    def onPress(self, key):
+
+        if key not in self.keysDown:
+            # when start recording, set self.currentTime to 0 and 
+            # set start time
+            press = self.listWidget.listWidgetAddStep(StepEnum.KEY, str(key))
+            self.lock.acquire()
+            self.keysDown[key] = (press, time.time())
+            self.lock.release()
+            if not self.updater.is_alive():
+                self.updater = Thread(target=self.update)
+                self.updater.start()
+
+        '''
+        for timeTup in self.keysDown.values():
+            timeTup[0].setText('%.2f' % (time.time() - timeTup[1]))
+        '''
+
         return True
 
-    def on_keyboard_event(self, event):
-        if self.window.recordButton.isChecked():
-            #self.window.listWidget.addItem(QListWidgetItem(chr(event.KeyID)))
-            self.window.listWidgetAddStep(StepEnum.ACTIVE_WAIT)
-            self.window.listWidgetAddStep(StepEnum.MOUSE_LEFT, (1, 2))
-            self.window.listWidgetAddStep(StepEnum.KEY)
-        return True            
 
-    def your_method(self):
-        self.new_wind = SubWindow()
-        self.new_wind.show()
+    def onRelease(self, key):
+        self.lock.acquire()
+        del self.keysDown[key]
+        self.lock.release()
+        return True
+
+    def update(self):
+        while len(self.keysDown) != 0:
+            self.lock.acquire()
+            for timeTup in self.keysDown.values():
+                timeTup[0].setText('%.2f' % (time.time() - timeTup[1]))
+            self.lock.release()
+
 
     def shutdown(self):
         PostQuitMessage(0)
