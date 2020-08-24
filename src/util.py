eecc73833737661e58b0d3ef26cc8eeac8997bae
John Li <john166@rocketmail.com>
2020-08-23 18:20:41 -0700

    almost there

--- src/util.py
+++ src/util.py
@@ -1,3 +1,5 @@
+"""File containing various utility functions."""
+
 from PyQt5 import QtWidgets, QtCore
 from StepConstants import StepEnum, keyConst
 from pynput.keyboard import KeyCode
@@ -5,6 +7,18 @@ import os
 
 
 def makeButton(parent, image, name='', x=0, y=0, width=30, height=30):
+    """Creates and returns a new button.
+
+    Args:
+        parent (QWidget): Widget to set as this button's parent.
+        image (str): CSS to display the button's image.
+        name (str): Name of this widget.
+        x (int): X coordinate in window to place the button
+        y (int): Y coordinate in window to place the button
+        width (int): Width of the button.
+        height (int): Height of the button.
+
+    """
     button = QtWidgets.QPushButton(parent)
     button.setGeometry(QtCore.QRect(x, y, width, height))
     button.setStyleSheet(image)
@@ -15,15 +29,40 @@ def makeButton(parent, image, name='', x=0, y=0, width=30, height=30):
     return button
 
 def synchronize(func):
+    """Decorator to call a function synchronously.
+
+    This synchrous call is with respect to what threading lock
+    blocks. This threading lock should be a member of the first
+    argument passed in of the function to be decorated.
+
+    Args:
+        func (func): Function to call synchronously.
+    """
     def sync_function(self, *args):
+        """Synchronous version of the function passed into the decorator.
+
+        Args:
+            *args: Aguments passed into the function being decorated.
+
+        """
         self.lock.acquire()
         func(self, *args)
         self.lock.release()
     return sync_function
 
 def read(filename, listWidget, recorder):
+    """Reads in a file containing macro data.
+
+    The read in macro data will also be used to initialize the list widget 
+    with macro widgets.
+
+    Args:
+        filename (str): Name of the file to read.
+
+    """
     if os.path.exists(filename):
-        # when optimizing, remember to open in binary mode for carriadge returns
+        # TODO when optimizing, remember to open in binary mode for carriadge 
+        # returns
         with open(filename, 'r') as toLoad:
             for line in toLoad:
                 name, steps, time, keys, keyString, loopNum = line.split('\0\0\0')
@@ -36,20 +75,43 @@ def read(filename, listWidget, recorder):
                 recorder.addHotkey(keys, steps, time, loopNum, recording=False)
 
 def write(filename, listWidget):
-    print('beginning write')
+    """Write the current macro data to a file.
+
+    Note that we delimit by null terminators because we could be writing
+    keys the user presesd, so we shouldn't delimit by any possible keyboard
+    character.
+
+    Args:
+        filename (str): Name of file to write to.
+        listWidget (QListWidget): List widget containing the data to be written.
+    """
     # maybe write to multiple files for each macro?
     with open(filename, 'w') as out:
         writable = listWidget.getMacroList(write=True)
+
         # Output buffer
         lines = []
         for toWrite in writable:
             name, steps, time, keys, keyString, loopNum = toWrite
             steps = _serializeSteps(steps) if steps else steps
+
+            # Delimit each hotkey key with a single null terminator
             keys = '\0'.join(map(lambda key: str(key), keys))
+
             lines.append(f'{name}\0\0\0{steps}\0\0\0{time}\0\0\0{keys}\0\0\0{keyString}\0\0\0{loopNum}\n')
         out.writelines(lines)
 
 def _serializeSteps(steps):
+    """Converts each step in the passed in list of steps to a string.
+
+    Each component of the step is delimited by a null terminator.
+
+    Args:
+        steps (list): List of tuples containing the data of each step
+                      of a macro.
+
+    Return: The passed in steps as a stirng.
+    """
     stepsStr = []
     for step in steps:
         stepType, data, hold, startTime = step
@@ -59,7 +121,17 @@ def _serializeSteps(steps):
 
 
 def _parseData(stepType, dataStr):
-    # if step type is key, then let data be the string
+    """Parses the passed in data string based on the passed in step type.
+
+    Args:
+        stepType (StepEnum): Type of this step to parse data for.
+        dataStr (str): String containing the data for this step.
+
+    Return: Parsed data.
+    """
+
+    # If the step type is a key press, then let data be the key's string
+    # representation.
     data = dataStr
     if stepType == StepEnum.ACTIVE_WAIT:
         data = None
@@ -81,6 +153,13 @@ def _parseData(stepType, dataStr):
     return data
 
 def _readSteps(stepsStr):
+    """Takes a string of serialized steps to read.
+
+    Args:
+        stepStr (str): String of serialized steps.
+
+    Return: List of steps parsed from the steps string.
+    """
     steps = stepsStr.split('\0\0')
     stepsList = []
     for step in steps:
@@ -93,15 +172,29 @@ def _readSteps(stepsStr):
     return stepsList
 
 def _readKeys(keysStr):
+    """Takes a string of serialized hotkey keys to read.
+
+    Args:
+        keysStr (str): String of seriealized hotkeys.
+
+    Return: Set of KeyCodes parsed from the keys string.
+    """
     keys = keysStr.split('\0')
     keySet = set()
+
     for key in keys:
+
+        #Special KeyCodes when converted to a string are formatted like
+        #KeyCode.<keycode> (e.g. KeyCode.ctrl). So we parse for just <keycode>.
         split = key.split('.')
         if len(split) > 1:
             keySet.add(keyConst(split[1]))
-        # else if virtual key code
+
+        # Else if virtual key code is surrounded by <>. Need to strip.
         elif key[0] == '<':
             keySet.add(KeyCode.from_vk(int(key[1:-1])))
+
+        # special case where character is a single quote
         else:
             # for some reason key[1:-1] doesn't work
             keySet.add(KeyCode.from_char(key.strip("'")
